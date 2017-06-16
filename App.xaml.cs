5f02da174401d3d7c2392d02dcb307852ad61e68
rs <spamnuker2000@yahoo.com>
2017-06-16 14:44:38 -0600

    Update App

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
+using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace NeoMedia
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
+	partial class App
 	{
+		void ShowExceptionMessage(Exception ex)
+		{
+			var message = "";
+			for (var ex2 = ex; ex2 != null; ex2 = ex2.InnerException)
+				message += $"{ex2.Message}\n";
+
+			var window = Current.Windows.OfType<Window>().SingleOrDefault(w => w.IsActive);
+			MessageBox.Show(window, message, "Error");
+
+#if DEBUG
+			if ((Debugger.IsAttached) && ((Keyboard.Modifiers & ModifierKeys.Shift) != ModifierKeys.None))
+			{
+				var inner = ex;
+				while (inner.InnerException != null)
+					inner = inner.InnerException;
+				var er = inner?.StackTrace?.Split('\r', '\n').FirstOrDefault(a => a.Contains(":line"));
+				if (er != null)
+				{
+					var idx = er.LastIndexOf(" in ");
+					if (idx != -1)
+						er = er.Substring(idx + 4);
+					idx = er.IndexOf(":line ");
+					er = $"{er.Substring(0, idx)} {er.Substring(idx + 6)}";
+					Clipboard.SetText(er);
+				}
+				Debugger.Break();
+			}
+#endif
+		}
+
+		public App()
+		{
+			DispatcherUnhandledException += App_DispatcherUnhandledException;
+			new MainWindow().Show();
+		}
+
+		void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			ShowExceptionMessage(e.Exception);
+			e.Handled = true;
+		}
 	}
 }
