69c2377c05fc0d1d1569e7a201c92e0cf73c62b8
rspackman <null@null>
2017-06-17 16:39:14 -0600

    Remove extra files

--- Site/app/orders.controller.js
+++
@@ -1,8 +0,0 @@
-(function () {
-	angular.module('app').controller('OrdersController', OrdersController);
-	OrdersController.$inject = ['$routeParams'];
-	function OrdersController($routeParams) {
-		var vm = this;
-		vm.customerId = $routeParams.id;
-	}
-})();
