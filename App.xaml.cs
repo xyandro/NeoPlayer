29b463089aa5c5e98bda5cd6d76ee8ca1f35a107
rs <spamnuker2000@yahoo.com>
2017-06-26 14:58:17 -0600

    Simplify

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,51 +1,18 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Windows;
-using System.Windows.Input;
-using System.Windows.Threading;
+﻿using System.Windows;
 
 namespace NeoRemote
 {
 	partial class App
 	{
-		void ShowExceptionMessage(Exception ex)
+		protected override void OnStartup(StartupEventArgs e)
 		{
-			var message = "";
-			for (var ex2 = ex; ex2 != null; ex2 = ex2.InnerException)
-				message += $"{ex2.Message}\n";
-
-			var window = Current.Windows.OfType<Window>().SingleOrDefault(w => w.IsActive);
-			MessageBox.Show(window, message, "Error");
-
-			if ((Settings.Debug) && (Debugger.IsAttached) && ((Keyboard.Modifiers & ModifierKeys.Shift) != ModifierKeys.None))
-			{
-				var inner = ex;
-				while (inner.InnerException != null)
-					inner = inner.InnerException;
-				var er = inner?.StackTrace?.Split('\r', '\n').FirstOrDefault(a => a.Contains(":line"));
-				if (er != null)
-				{
-					var idx = er.LastIndexOf(" in ");
-					if (idx != -1)
-						er = er.Substring(idx + 4);
-					idx = er.IndexOf(":line ");
-					er = $"{er.Substring(0, idx)} {er.Substring(idx + 6)}";
-					Clipboard.SetText(er);
-				}
-				Debugger.Break();
-			}
+			base.OnStartup(e);
+			new MainWindow().Show();
 		}
 
 		public App()
 		{
-			DispatcherUnhandledException += App_DispatcherUnhandledException;
-		}
-
-		void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-		{
-			ShowExceptionMessage(e.Exception);
-			e.Handled = true;
+			DispatcherUnhandledException += (s, e) => e.Handled = true;
 		}
 	}
 }
