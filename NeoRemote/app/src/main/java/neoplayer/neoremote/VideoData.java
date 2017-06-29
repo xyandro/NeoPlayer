d01ee8eaf3c178c9e51807a6f0b8b581718110b2
rs <randon_s@yahoo.com>
2017-06-28 23:07:53 -0600

    Added misc videos

---
+++ NeoRemote/app/src/main/java/neoplayer/neoremote/VideoData.java
@@ -0,0 +1,13 @@
+package neoplayer.neoremote;
+
+public class VideoData {
+    public String Name;
+    public String Description;
+    public boolean Selected;
+
+    public VideoData(String name, String description, boolean selected) {
+        Name = name;
+        Description = description;
+        Selected = selected;
+    }
+}
--- NeoRemote/app/src/main/java/neoplayer/neoremote/VideosFragment.java
+++ NeoRemote/app/src/main/java/neoplayer/neoremote/VideosFragment.java
@@ -1,71 +1,92 @@
 package neoplayer.neoremote;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class VideosFragment extends Fragment {
-    private static final String TAG = MainActivity.class.getSimpleName();
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View result = inflater.inflate(R.layout.fragment_videos, container, false);
 
         ListView listView = result.findViewById(R.id.videosList);
-        ArrayList<String> listItems = new ArrayList<String>();
-        listItems.add("Randon");
-        listItems.add("Ben");
-        listItems.add("Sophie");
-        listItems.add("Timothy");
-        listItems.add("Katelyn");
-        listItems.add("Phoebe");
-        listItems.add("Megan");
-        listItems.add("Randon");
-        listItems.add("Ben");
-        listItems.add("Sophie");
-        listItems.add("Timothy");
-        listItems.add("Katelyn");
-        listItems.add("Phoebe");
-        listItems.add("Megan");
-        listItems.add("Randon");
-        listItems.add("Ben");
-        listItems.add("Sophie");
-        listItems.add("Timothy");
-        listItems.add("Katelyn");
-        listItems.add("Phoebe");
-        listItems.add("Megan");
-        listItems.add("Randon");
-        listItems.add("Ben");
-        listItems.add("Sophie");
-        listItems.add("Timothy");
-        listItems.add("Katelyn");
-        listItems.add("Phoebe");
-        listItems.add("Megan");
-        listItems.add("Randon");
-        listItems.add("Ben");
-        listItems.add("Sophie");
-        listItems.add("Timothy");
-        listItems.add("Katelyn");
-        listItems.add("Phoebe");
-        listItems.add("Megan");
-        listItems.add("Randon");
-        listItems.add("Ben");
-        listItems.add("Sophie");
-        listItems.add("Timothy");
-        listItems.add("Katelyn");
-        listItems.add("Phoebe");
-        listItems.add("Megan");
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, listItems);
+        ArrayList<VideoData> listItems = new ArrayList<VideoData>();
+
+        listItems.add(new VideoData("Randon Spackman", "Randon", false));
+        listItems.add(new VideoData("Ben Christensen", "Ben", true));
+        listItems.add(new VideoData("Sophie Christensen", "Sophie", true));
+        listItems.add(new VideoData("Timothy Christensen", "Timothy", false));
+        listItems.add(new VideoData("Katelyn Spackman", "Katelyn", true));
+        listItems.add(new VideoData("Phoebe Christensen", "Phoebe", true));
+        listItems.add(new VideoData("Megan Spackman", "Megan", true));
+        listItems.add(new VideoData("Randon Spackman", "Randon", false));
+        listItems.add(new VideoData("Ben Christensen", "Ben", true));
+        listItems.add(new VideoData("Sophie Christensen", "Sophie", true));
+        listItems.add(new VideoData("Timothy Christensen", "Timothy", false));
+        listItems.add(new VideoData("Katelyn Spackman", "Katelyn", true));
+        listItems.add(new VideoData("Phoebe Christensen", "Phoebe", true));
+        listItems.add(new VideoData("Megan Spackman", "Megan", true));
+        listItems.add(new VideoData("Randon Spackman", "Randon", false));
+        listItems.add(new VideoData("Ben Christensen", "Ben", true));
+        listItems.add(new VideoData("Sophie Christensen", "Sophie", true));
+        listItems.add(new VideoData("Timothy Christensen", "Timothy", false));
+        listItems.add(new VideoData("Katelyn Spackman", "Katelyn", true));
+        listItems.add(new VideoData("Phoebe Christensen", "Phoebe", true));
+        listItems.add(new VideoData("Megan Spackman", "Megan", true));
+        listItems.add(new VideoData("Randon Spackman", "Randon", false));
+        listItems.add(new VideoData("Ben Christensen", "Ben", true));
+        listItems.add(new VideoData("Sophie Christensen", "Sophie", true));
+        listItems.add(new VideoData("Timothy Christensen", "Timothy", false));
+        listItems.add(new VideoData("Katelyn Spackman", "Katelyn", true));
+        listItems.add(new VideoData("Phoebe Christensen", "Phoebe", true));
+        listItems.add(new VideoData("Megan Spackman", "Megan", true));
+        listItems.add(new VideoData("Randon Spackman", "Randon", false));
+        listItems.add(new VideoData("Ben Christensen", "Ben", true));
+        listItems.add(new VideoData("Sophie Christensen", "Sophie", true));
+        listItems.add(new VideoData("Timothy Christensen", "Timothy", false));
+        listItems.add(new VideoData("Katelyn Spackman", "Katelyn", true));
+        listItems.add(new VideoData("Phoebe Christensen", "Phoebe", true));
+        listItems.add(new VideoData("Megan Spackman", "Megan", true));
+
+        final VideosListAdapter adapter = new VideosListAdapter(getContext(), listItems);
         listView.setAdapter(adapter);
 
         return result;
+    }
+
+    private class VideosListAdapter extends ArrayAdapter<VideoData> {
+        private final Context context;
+        private final ArrayList<VideoData> list;
+
+        public VideosListAdapter(Context context, ArrayList<VideoData> list) {
+            super(context, -1, list);
+            this.context = context;
+            this.list = list;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.fragment_videos_listitem, parent, false);
+
+            VideoData videoData = list.get(position);
+
+            ImageView imageView = rowView.findViewById(R.id.image);
+            imageView.setImageResource(videoData.Selected ? R.drawable.check : R.drawable.uncheck);
+
+            TextView textView = rowView.findViewById(R.id.name);
+            textView.setText(videoData.Description);
+
+            return rowView;
+        }
     }
 }
